661e446a31b59263a8b1a5ae256f1eb079cf8468
texasdon28 <126028439+texasdon28@users.noreply.github.com>
2025-06-30 16:33:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import { PieChart, Pie, Cell, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 interface Transaction {
